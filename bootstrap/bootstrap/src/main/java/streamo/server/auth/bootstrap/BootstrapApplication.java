44e34a4e0cf48505d405fc7c89de1a11327abfec
akhilpgvr <akhilpgvr@gmail.com>
2022-12-22 11:05:14 +0530

    up

--- bootstrap/bootstrap/src/main/java/streamo/server/auth/bootstrap/BootstrapApplication.java
+++ bootstrap/bootstrap/src/main/java/streamo/server/auth/bootstrap/BootstrapApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootstrapApplication {
-//ge
+//geaa
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
 	}
